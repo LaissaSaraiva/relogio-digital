511910aab698624d882cf50207c90f7e9f44df58
Laissa Saraiva <laissa_saraiva@hotmail.com>
2022-07-26 19:12:32 -0300

    Inclusão da do campo com dia/mês/ano

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,18 @@ const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
+const dia = document.getElementById("data");
+
 const relogio = setInterval(function tempo() {
   let dataAtual = new Date();
   let horaAtual = dataAtual.getHours();
   let minutoAtual = dataAtual.getMinutes();
   let segundoAtual = dataAtual.getSeconds();
+
+  let diaAtual = dataAtual.getDate();
+  let mesAtual =  (dataAtual.getMonth() 
+  + 1);
+  let anoAtual = dataAtual.getFullYear();
 
   if (horaAtual < 10) {
     horaAtual = "0" + horaAtual;
@@ -20,8 +27,18 @@ const relogio = setInterval(function tempo() {
     segundoAtual = "0" + segundoAtual;
   }
 
+  if(diaAtual < 10) {
+    diaAtual = "0" + diaAtual;
+  }
+
+  if(mesAtual < 10) {
+    mesAtual = "0" + mesAtual;
+  }
+  
   horas.textContent = horaAtual;
   minutos.textContent = minutoAtual;
   segundos.textContent = segundoAtual;
-  
+
+  dia.textContent = `${diaAtual} . ${mesAtual} . ${anoAtual}`
 });
+
